de5b163c146a4e7089f09d2e2167fa0023e9b5c5
Showunmi Segun <showunmioluwasegun135@gmail.com>
2026-02-23 13:46:09 +0100

    feat: Add and style conditional social media links (X and GitHub) to the account settings header.

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -290,6 +290,38 @@ const AccountSettings = ({
                           <h2 className="header-name">{name || "User Name"}</h2>
                           <p className="header-email">{user?.email}</p>
                           <p className="header-phone">{phone || "+234 00 0000 0000"}</p>
+                          
+                          {/* Connected Social Apps Icons */}
+                          {(twitterConnected || githubConnected) && (
+                            <div className="header-social-links">
+                              {twitterConnected && twitterUsername && (
+                                <a 
+                                  href={`https://x.com/${twitterUsername}`} 
+                                  target="_blank" 
+                                  rel="noopener noreferrer"
+                                  className="header-social-icon x-header-icon"
+                                  title={`X: @${twitterUsername}`}
+                                >
+                                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                                  </svg>
+                                </a>
+                              )}
+                              {githubConnected && githubUsername && (
+                                <a 
+                                  href={`https://github.com/${githubUsername}`} 
+                                  target="_blank" 
+                                  rel="noopener noreferrer"
+                                  className="header-social-icon github-header-icon"
+                                  title={`GitHub: ${githubUsername}`}
+                                >
+                                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.45-1.15-1.11-1.46-1.11-1.46-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 10 0 0 12 2z"/>
+                                  </svg>
+                                </a>
+                              )}
+                            </div>
+                          )}
                       </motion.div>
                   ) : (
                        /* Edit Mode Inputs (Header) */
