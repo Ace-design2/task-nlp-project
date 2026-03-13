ede0897ed0dbd83ba2971e39282ca3f97db21225
Showunmi Segun <showunmioluwasegun135@gmail.com>
2026-03-13 21:04:49 +0100

    fix: shorten voice input silence detection timeout from 2500ms to 1200ms.

--- src/components/VoiceInput.js
+++ src/components/VoiceInput.js
@@ -197,7 +197,7 @@ export default function VoiceInput({
 
       silenceTimerRef.current = setTimeout(() => {
           submitText();
-      }, 2500);
+      }, 1200);
     };
 
     recognition.onerror = (event) => {
